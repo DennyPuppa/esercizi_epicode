502d7cc82cb49446df87d3ac0201d331058d1757
DennyPuppa <63900736+DennyPuppa@users.noreply.github.com>
2024-01-16 21:05:39 +0100

    Add exam page

---
+++ BUILDWEEK/js/script.js
@@ -0,0 +1,6 @@
+//variabile e funzione per iniziare esame
+const proceed = document.querySelector("#btn-exam")
+
+proceed.addEventListener('click', () => {
+    window.location.href = "exam.html";
+})
