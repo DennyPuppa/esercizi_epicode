6bec36c2f0db19ffb096b936e7e59036a34fb267
DennyPuppa <63900736+DennyPuppa@users.noreply.github.com>
2024-01-19 20:54:10 +0100

    Add final

--- BUILDWEEK/js/script.js
+++ BUILDWEEK/js/script.js
@@ -1,27 +1,28 @@
 let questionIndex = 0; // da 1 a 10
 let clockSeconds = 6; // valore a caso, possiamo lasciare un minuto o decidere di cambiare a seconda della difficoltà
 let score = 0;       //incrementa ad ogni risposta corretta
-
+const timeRange = 6;
 // populateQuestion()
 
 nextQuestion()
+clock()
 
 function nextQuestion() {
     if (questionIndex < questions.length) {
 
-        clock()
+        clockSeconds = timeRange;
+        // clock()
         populateQuestion()
         const buttons = document.querySelectorAll(".btn-answer");
         buttons.forEach((button)=>{
             button.addEventListener("click", ()=>{
-                // console.log(e.target);
+                // console.log(event);
                 console.log(button.textContent);
                 controlAnswer(button.textContent);
                 questionIndex++;
                 console.log("questionIndex = " + questionIndex);
                 // stopClock();
-                // nextQuestion();
-                clockSeconds = 0;
+                nextQuestion();
                 
             })
         })
@@ -33,26 +34,24 @@ function nextQuestion() {
 }
 
 function populateQuestion() {
+    
     cleanWindow()
+    cleanClock()
     // clock()
     const questionContainer = document.querySelector("#question-container");
     const questionTitle = document.createElement("h1");
     questionTitle.textContent = questions[questionIndex].question;
     questionContainer.append(questionTitle);
 
-    // const answerBtn = document.querySelectorAll(".btn-answer");
-
-    // const arrayBtn = Array.from(answerBtn);
-    // console.log(arrayBtn);
     populateBtns()
     // genere e popola 4 o 2 bottoni in base all'indice dell array questions
-    // riavvia il clock
+    counterQuestions()
 }
 
 function populateBtns(){
     let answerData = questions[questionIndex].incorrect_answers;
     answerData.push(questions[questionIndex].correct_answer)
-    console.log(answerData);
+    // console.log(answerData);
     const btnContainer = document.querySelector(".btn-container");
 
     const shuffledAnswer = shuffleAnswer(answerData);
@@ -63,6 +62,13 @@ function populateBtns(){
         btn.textContent = answer;
         btnContainer.append(btn);
     });
+}
+
+function counterQuestions (){
+    const counterQuesions = document.querySelector(".count-question-container");
+    const textCounterQuestions = document.createElement("p");
+    textCounterQuestions.textContent = "QUESTION " + (questionIndex + 1) + "/" + questions.length;
+    counterQuesions.append(textCounterQuestions); 
 }
 
 function shuffleAnswer(answerArray){
@@ -85,47 +91,89 @@ function cleanWindow() {
     //pulisce la pagina da elementi variabili
     const titleContainer = document.querySelector("#question-container");
     const answersContainer = document.querySelector("#answers-container");
+    const counterQuesions = document.querySelector(".count-question-container");
+    
+    
     
     titleContainer.textContent = "";
     answersContainer.textContent = "";
+    counterQuesions.textContent = "";
+    
 
+}
+
+function cleanClock(){
+    const clockText = document.querySelector(".clock-text");
+    if(clockText){
+        clockText.textContent = "";
+    }
+    // if(questionIndex === questions.length){
+    //     const clockContainer = document.querySelector(".clock");
+    //     clockContainer.textContent = "";
+    // }
 }
 
 function clock() {  // scandisce solo il tempo usando la variabile globale clockSeconds
     //setInterval( ,1000);
-    clockSeconds = 6;
+    // clockSeconds = 6;
     const countdown = setInterval(() => {
+        if (questionIndex === questions.length) {
+            // console.log("prova");
+            const clockContainer = document.querySelector(".clock");
+            clockContainer.textContent = "";
+            clearInterval(countdown)
+            
+        }    
+        cleanClock()        
         clockSeconds --
+        showClock()
         console.log(clockSeconds);
-        if (clockSeconds <= 0){
-            if (questionIndex < questions.length) {
-                clearInterval(countdown)
-            }
+        
+        if (clockSeconds === 0){
+            
             // clearInterval(countdown);
             questionIndex++;
             console.log("questionIndex = " + questionIndex);
-            clockSeconds = 6;
+            clockSeconds = timeRange;
             nextQuestion()
     }}, 1000);
-    console.log("punteggio: " + score);
+    // console.log("punteggio: " + score);
+}
+
+function showClock(){
+    const clockContainer = document.querySelector(".clock");
+    const clockText = document.querySelector(".clock-text");
+    const seconds = document.createElement("p");
+    const timerClock = document.createElement("p");
+    timerClock.classList.add("timer-clock");
+    const remaining = document.createElement("p");
+
+    seconds.textContent = "SECONDS";
+    timerClock.textContent = clockSeconds;
+    remaining.textContent = "REMAINING";
+
+        if (clockText) {
+        clockContainer.append(clockText);
+        clockText.append(seconds, timerClock, remaining);
+    }
 }
 
 function showResults() {
     cleanWindow()
+    // const clockContainer = document.querySelector(".clock");
+    // clockContainer.remove();
     // elimina gli elementi popolati da populateQuestion e genere un output html con score
-    // const mainContainer = document.querySelector("#bigger-container");
-    // const circle = document.createElement("div");
-    // const resultsContainer = document.createElement("div");
-    // const resultsText = document.createElement("p");
-    // resultsContainer.classList.add("results-container");
+    const mainContainer = document.querySelector("#bigger-container");
+    const circle = document.createElement("div");
+    const resultsContainer = document.createElement("div");
+    const resultsText = document.createElement("p");
+    resultsContainer.classList.add("results-container");
 
-    // resultsText.innerHTML = score + "/" + questions.length;
+    resultsText.innerHTML = score + "/" + questions.length;
 
-    // mainContainer.append(circle);
-    // circle.append(resultsContainer);
-    // resultsContainer.append(resultsText);
-
-
+    mainContainer.append(circle);
+    circle.append(resultsContainer);
+    resultsContainer.append(resultsText);
 }
 
 
