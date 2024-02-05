385da2a6bc4b3d824d73b740c4dc8c18c9e71931
DennyPuppa <63900736+DennyPuppa@users.noreply.github.com>
2024-02-05 18:43:19 +0100

    Push exercise (not finished)

---
+++ M3W2D4/js/script.js
@@ -0,0 +1,22 @@
+function countTravel(){
+    const travelNumbers = document.querySelectorAll(".card")
+    
+    let index = 0
+    travelNumbers.forEach(travel => {
+        index ++;
+    });
+    console.log(index);
+}
+
+countTravel()
+
+function removeCard{
+    const allCards = document.querySelectorAll(".card")
+}
+
+function createModal(){
+    const btnContact = document.querySelector("#btnContact");
+    btnContact.addEventListener("click", () => {
+    
+})
+}
